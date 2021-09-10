964b0d89ca3d9479e9a390a638addc03548d09d4
Jens <jdisselhoff@gmail.com>
2021-09-10 15:10:13 +0100

    Add max-width for super wide screen

--- pages/index.vue
+++ pages/index.vue
@@ -1,74 +1,81 @@
 <template lang="pug">
-  div
+  .bg-gray-lightest
     div(class=`
-       bg-gray-dark flex justify-left px-3 pt-3 pb-2
-       md:justify-between md:items-center
+      max-w-screen-2xl
+      m-auto
     `)
-      img(:src="require('@/public/images/Logo-light.png')" class=`
-        items-center
-        w-1/4 md:w-28
-        my-2
+      div(class=`
+         bg-gray-dark flex justify-left px-3 pt-3 pb-2
+         md:justify-between md:items-center
+      `)
+        img(:src="require('@/public/images/Logo-light.png')" class=`
+          items-center
+          w-1/4 md:w-28
+          my-2
+        `)
+        div(class=`
+          md:block
+           hidden flex items-center
+        `)
+          a(href="mailto:hello@infectic.com" target="_blank")
+            div(class=`
+              rounded-lg py-2 px-6 text-red-lightest bg-gradient-to-r from-red to-gray
+              hover:border-2
+            `) Let's Talk
+      div(class=`
+         bg-gradient-to-r from-red-lightest to-gray h-1
       `)
       div(class=`
-        md:block
-         hidden flex items-center
+         relative bg-gray-darkest
+         before:bg-gradient-to-t before:from-gray-dark
+         before:left-0 before:top-0 before:right-0 before:bottom-0
+         md:before:hidden before:absolute
       `)
-        a(href="mailto:hello@infectic.com" target="_blank")
+        img(:src="require('@/public/images/face-bg.png')" class=`
+          bg-gradient-to-t from-gray
+          w-full
+          md:ml-auto md:mr-0
+          md:w-1/2 md:align-left
+          lg:w-5/12
+        `)
+        div(class=`
+           absolute bottom-0 left-0 right-0 z-10 md:top-1/3
+        `)
           div(class=`
-            rounded-lg py-2 px-6 text-red-lightest bg-gradient-to-r from-red to-gray
-            hover:border-2
-          `) Let's Talk
-    div(class=`
-       bg-gradient-to-r from-red-lightest to-gray h-1
-    `)
-    div(class=`
-       relative bg-gray-darkest
-       before:bg-gradient-to-t before:from-gray-dark
-       before:left-0 before:top-0 before:right-0 before:bottom-0
-       md:before:hidden before:absolute
-    `)
-      img(:src="require('@/public/images/face-bg.png')" class=`
-        bg-gradient-to-t from-gray
-        w-full
-        md:ml-auto md:mr-0
-        md:w-1/2 md:align-left
-      `)
+            md:top-50
+            md:pl-5
+            lg:pl-9
+          `)
+            div(class=`
+               text-red text-6xl text-center w-full pb-0
+               md:text-left md:text-8xl
+            `) Contagiously
+            div(class=`
+               text-red-lightest text-8xl text-center w-full
+               md:text-left md:text-9xl
+            `) Effective
       div(class=`
-         absolute bottom-0 left-0 right-0 z-10 md:inset-y-1/3
+        bg-black
+        md:hidden
       `)
         div(class=`
-          md:top-50
+           flex items-center p-3
         `)
-          div(class=`
-             text-red text-6xl text-center w-full pb-0
-             md:text-left md:text-8xl
-          `) Contagiously
-          div(class=`
-             text-red-lightest text-8xl text-center w-full
-             md:text-left md:text-9xl
-          `) Effective
-    div(class=`
-      bg-black
-      md:hidden
-    `)
-      div(class=`
-         flex items-center p-3
-      `)
-        a(href="mailto:hello@infectic.com" target="_blank" class=`
-          m-auto
-          my-3
-        `)
-          div(class=`
-            rounded-lg py-3 px-10 text-red-lightest bg-gradient-to-r from-red to-gray
-            text-xl
-          `) Let's Talk
-    div.bg-gradient-to-r.from-red-lightest.to-gray.h-1
-    div.bg-gray-dark.py-6
-      div(class=`
-        flex
-        flex-col md:flex-row mx-1
-       `)
-          SkillCard(v-for="skill in skills" :title="skill.title" :text="skill.text" :key="skill.title")
+          a(href="mailto:hello@infectic.com" target="_blank" class=`
+            m-auto
+            my-3
+          `)
+            div(class=`
+              rounded-lg py-3 px-10 text-red-lightest bg-gradient-to-r from-red to-gray
+              text-xl
+            `) Let's Talk
+      div.bg-gradient-to-r.from-red-lightest.to-gray.h-1
+      div.bg-gray-dark.py-6
+        div(class=`
+          flex
+          flex-col md:flex-row mx-1
+         `)
+            SkillCard(v-for="skill in skills" :title="skill.title" :text="skill.text" :key="skill.title")
   //- div
   //-   div.bg-gray-dark.flex.justify-between.px-6.pt-3.pb-2
   //-     img(:src="require('@/public/images/Logo-light.png')").items-center
